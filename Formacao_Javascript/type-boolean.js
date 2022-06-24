3559cace49c521e104094ad46c1418d9d3e119e7
Mateus Almeida <mateusvr5@gmail.com>
2022-06-24 08:40:36 -0300

    Just a commit from alura course

---
+++ Formacao_Javascript/type-boolean.js
@@ -0,0 +1,5 @@
+const verdadeiro = true;
+
+if(verdadeiro){
+    console.log("Verdadeiro");
+}
---
+++ Formacao_Javascript/type-number.js
@@ -0,0 +1,8 @@
+const meuNumero = 3;
+
+const primeiroNumero = 2;
+const segundoNumero = 4;
+
+const operacao = primeiroNumero + segundoNumero;
+
+console.log(operacao);
