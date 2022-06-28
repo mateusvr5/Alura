83c891a836d8c5cc174391d7e1318f121c7f626a
Mateus Almeida <mateusvr5@gmail.com>
2022-06-28 11:07:49 -0300

    Just being a programmer

--- Formacao_Javascript/script.js
+++ Formacao_Javascript/script.js
@@ -1,32 +1,6 @@
-//var
+// tipagem dinâmica
+// untyped
 
-//var altura = 5;
-//var comprimento = 7;
-//area = altura * comprimento;
-//console.log(area);
-
-//let forma = 'Retângulo';
-//let altura = 5;
-//let comprimento = 7;
-//let area;
-
-//if(forma === 'Retângulo'){
-//    area = altura * comprimento;
-//}else {
-//    area = (altura * comprimento) / 2;
-//}
-
-//console.log(area);
-
-const forma = 'quadrado';
-const altura = 5;
-const comprimento = 7;
-let area;
-
-if(forma === 'quadrado'){
-    area = altura * comprimento;
-}else{
-    area = (altura * comprimento) / 2;
-}
-
-console.log(area);
+let minhaVar = 567;
+minhaVar = "texto";
+minhaVar = true;
