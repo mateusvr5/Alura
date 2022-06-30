1b462ba4cec733085b112faad347df9e01e2455a
Mateus Almeida <mateusvr5@gmail.com>
2022-06-30 08:40:26 -0300

    Just a commit from Alura courses

---
+++ Formacao_Javascript/template-strings.js
@@ -0,0 +1,16 @@
+// const nome = "Ju";
+// const idade = 2021-1981;
+// const cidadeDeNascimento = "São Paulo";
+
+// // const apresentacao = "meu nome é " + nome + ", minha idade é " + idade
+// // + " e nasci na cidade de " + cidadeDeNascimento;
+
+// const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+// console.log(apresentacao);
+
+const a = [];
+
+if(a == 0){
+    console.log('Verdade');
+}
